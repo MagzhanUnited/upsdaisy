e6947f33948d1e67f0b7a2e5ee20b81850280a5d
Magzhan_Zhumabaev <123943870+MagzhanUnited@users.noreply.github.com>
2023-10-21 20:00:46 +0600

    I said what I said

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
 
 func main() {
-	fmt.Printf("Sanxhar")
+	// Replace "YOUR_BOT_API_TOKEN" with the token you received from the BotFather.
+	bot, err := tgbotapi.NewBotAPI("6517147428:AAG_OkAcpjQOolOjzXko3rjvE4lcwtFFWhU")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	// Create an update configuration with an offset.
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 60
+	// Continuously fetch updates using long polling.
+	updates, err := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		fmt.Println("update.Message.Text:", update.Message.Text)
+		// Reply to the user's message.
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "SUIIIII")
+		_, err := bot.Send(msg)
+		if err != nil {
+			log.Panic(err)
+		}
+	}
 }
