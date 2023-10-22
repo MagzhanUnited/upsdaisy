020b7fb03eebe9f101f0e4650dcefc21dbfcece8
Magzhan_Zhumabaev <123943870+MagzhanUnited@users.noreply.github.com>
2023-10-22 21:47:53 +0600

    telegram bot

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	handler "mytelegrambot/Handler"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func main() {
 	// Replace "YOUR_BOT_API_TOKEN" with the token you received from the BotFather.
-	bot, err := tgbotapi.NewBotAPI("6517147428:AAG_OkAcpjQOolOjzXko3rjvE4lcwtFFWhU")
+	bot, err := tgbotapi.NewBotAPI("6614980196:AAGjdnaxpNgybhFJQA_blvPL3OQjHEMuYd8")
 	if err != nil {
 		log.Panic(err)
 	}
@@ -28,12 +30,38 @@ func main() {
 		if update.Message == nil {
 			continue
 		}
+
+		handleCommands(bot, update.Message)
+
 		fmt.Println("update.Message.Text:", update.Message.Text)
-		// Reply to the user's message.
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "SUIIIII")
-		_, err := bot.Send(msg)
-		if err != nil {
-			log.Panic(err)
+		if isYouTubeURL(update.Message.Text) {
+			audioBytes, audioextraction := handler.ServeAudio(handler.VideoData{Videourl: update.Message.Text})
+			audioFile := tgbotapi.FileBytes{Name: audioextraction.AudioName, Bytes: audioBytes}
+			audio := tgbotapi.NewAudioUpload(update.Message.Chat.ID, audioFile)
+			_, err := bot.Send(audio)
+			if err != nil {
+				log.Panic(err)
+			}
+		}
+
+	}
+}
+
+func handleCommands(bot *tgbotapi.BotAPI, msg *tgbotapi.Message) {
+	command := msg.Command()
+
+	switch command {
+	case "start":
+		msg := tgbotapi.NewMessage(msg.Chat.ID, "Ютуб видеоның ссылкасын жібер")
+		bot.Send(msg)
+	default:
+		if !isYouTubeURL(msg.Text) {
+			msg := tgbotapi.NewMessage(msg.Chat.ID, "Мен сізді түсінбедім. бастау үшін /start деп жазыңыз.")
+			bot.Send(msg)
 		}
 	}
+}
+
+func isYouTubeURL(text string) bool {
+	return strings.HasPrefix(text, "https://youtu.be/")
 }
